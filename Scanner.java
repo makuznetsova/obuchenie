aa3101fe798f40a44bf0e6226d59e750fc647b46
ma.kuznetsova <you@example.com>
2025-02-12 14:14:09 +0300

    commit Java class

---
+++ Scanner.java
@@ -0,0 +1,15 @@
+//public class Scanner {
+//    public int nextDouble(int bodyTemperature, int nextDouble) {
+//         return nextDouble;
+//    }
+//}
+
+
+//public class Scanner {
+//    public int nextInt(int billet, int family) {
+//        int nextInt = 3;
+//        int temperature = 3;
+//        return nextInt;
+//    }
+//}
+
