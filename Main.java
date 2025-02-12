e6a0a63620342c18be845da2d27b0faa55d99d4b
ma.kuznetsova <you@example.com>
2025-02-12 14:12:43 +0300

    commit Java

--- Main.java
+++ Main.java
@@ -1,19 +1,119 @@
-public class Main {
-    public static void main(String[] args) {
 
-        int eaters = 5; // сколько людей будут есть
+//1 ЗАДАНИЕ
 
+//public class Main {
+//    public static void main(String[] args) {
+//        int intValue = 1;
+//        int numericValue = 2;
+//        int size = 3;
+//        int number = 4;
+//        System.out.println(intValue);
+//        System.out.println(numericValue);
+//        System.out.println("Размер :" + size);
+//        System.out.println(number);
+//    }
+//}
+кп
+
+//2 ЗАДАНИЕ
+
+//public class Main {
+//    public static void main(String[] args) {
+//        int A = 4;
+//        int B = 199;
+//        System.out.println("RESUME");
+//        System.out.println(("TITLE: " + A));
+//        System.out.println(("DEGREE: " + B));
+//    }
+//}
+
+//public class Main {
+//    public static void main(String[] args) {
+//        int A = 4;
+//        int B = 199;
+//        System.out.println("RESUME".toLowerCase());
+//        System.out.println(("TITLE: " + A).toLowerCase());
+//        System.out.println(("DEGREE: " + B).toLowerCase());
+//    }
+//}
+
+
+
+import java.util.Scanner;
+//
+//3 ЗАДАНИЕ
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        double temperature = scanner.nextDouble();
+//
+//        if (temperature < 0) {
+//            System.out.println("на улице холодно");
+//        } else {
+//            System.out.println("на улице тепло");
+//        }
+//    }
+//}
+
+
+//4 ЗАДАНИЕ
+
+//public class Main {
+//    private static boolean isHigh;
+//    private static boolean isLow;
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        double bodyTemperature = scanner.nextDouble();
+//        isHigh = bodyTemperature > 37; // принимает значение да или нет по условию
+//        isLow = bodyTemperature < 36; // принимает значение да или нет по условию
+//        if (isHigh) // готовое да или нет
+//            System.out.println("Температура тела высокая");
+//        else if (isLow) // готовое да или нет
+//            System.out.println("Температура тела низкая");
+//        else
+//            System.out.println("Температура тела нормальная");
+//
+//    }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+
+/*        int eaters = 5; // сколько людей будут есть
         int water = 3000; // миллилитров воды
         int potatoes = 5; // картофелин
         double chicken = 6; // куриных бёдер
         int spices = 10; // ложек специй
 
-        System.out.println("Сереварили суп. На одного человека вышло:");
-        System.out.println((water / eaters) + " миллилитров(а) воды");
+        int intValue = 1;
+        int numericValue = 2;
+        int size = 3;
+        int number = 4;
+
+        System.out.println(("СВАРИЛИ СУП. НА ОДНОГО ЧЕЛОВЕКА ВЫШЛО").toLowerCase());
+        System.out.println(((water / eaters) + " МИЛЛИЛИТРОВ ВОДЫ").toLowerCase());
         System.out.println((potatoes / eaters) + " картофелин(а/ы)");
         System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ки/ка) специй");
+        System.out.println (intValue);
+        System.out.println (numericValue);
+        System.out.println (size);
+        System.out.println (number);*/
 
-    }
-}
-впвап
+//    }
+//}
